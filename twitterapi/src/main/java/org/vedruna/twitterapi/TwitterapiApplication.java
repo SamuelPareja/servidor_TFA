45482676b17a7e91f23b63c671edaedd506a39b9
Samuel Pareja <zamvel_1@hotmail.com>
2025-10-29 13:55:51 +0100

    Spring Boot project added

---
+++ twitterapi/src/main/java/org/vedruna/twitterapi/TwitterapiApplication.java
@@ -0,0 +1,13 @@
+package org.vedruna.twitterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterapiApplication.class, args);
+	}
+
+}
---
+++ twitterapi/src/test/java/org/vedruna/twitterapi/TwitterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vedruna.twitterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
